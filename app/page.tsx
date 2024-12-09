91b9049d18bd49262f95898c8e766d34e21478c6
NatawatDev <natwat.codealong@gmail.com>
2024-12-09 23:55:55 +0700

    add: banner swiper and useFetch hook

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+'use client'
 
-const page = () => {
+import { useState, useEffect } from 'react'
+import Banner from '@/components/Banner'
+import ProductContent from '@/components/ProductContent'
+import useFetch from '@/utils/useFetch'
+import { BannerItem } from '@/types/index'
+
+const Page = () => {
+  const [bannerList, setBannerList] = useState<BannerItem[]>([])
+
+  const { data, loading, error } = useFetch<BannerItem[]>('/banners')
+  
+  useEffect(() => {
+    if (data) {
+      setBannerList(data)
+    }
+   
+  }, [data])
+
+  if (loading) return <div>Loading...</div>
+  
+  if (error) return <div>Error: {error}</div>
+
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <Banner bannerList={bannerList} />
+      <ProductContent />
+    </div>
+  );
+};
 
-export default page
+export default Page;
